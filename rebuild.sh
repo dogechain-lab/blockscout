4d904e5a8dab7a950dabfd6fae1f939bf51c5106
0xcb9ff9 <0xcb9ff9@proton.me>
2023-07-01 12:00:41 +0800

    update banner style

--- rebuild.sh
+++ rebuild.sh
@@ -4,6 +4,5 @@ mix do deps.get, local.rebar --force, deps.compile, compile
 cd apps/block_scout_web/assets; npm install && node_modules/webpack/bin/webpack.js --mode production; cd -
 cd apps/explorer && npm install; cd -
 mix phx.digest
-cd apps/block_scout_web; mix phx.gen.cert blockscout blockscout.local; cd -
 
 
